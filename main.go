6141d5d450809dfc55bb1a8d9488835dc76c1702
temoorx <taak140@gmail.com>
2024-02-05 21:41:28 +0500

    check domain function added

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main(){
-	
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain, haxMx, hasSPF, sprRecord, hasDMARC, demarcRecord \n")
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: Could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+	var hasMx, hasSPF, hasDMARC bool
+	var spfRecord, demarcRecord string
+
+	maxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error :%v\n", err)
+	}
+
+	if len(maxRecords) > 0 {
+		hasMx = true
+
+	}
+
 }
